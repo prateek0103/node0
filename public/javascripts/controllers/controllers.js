0130ac5d302b5b916fdf3ff3a9a0a8e5709f65d3
Prateek Gupta <aryan.838@gmail.com>
2015-06-29 12:34:16 +0530

    hello world fixed

--- public/javascripts/controllers/controllers.js
+++ public/javascripts/controllers/controllers.js
@@ -1,5 +1,3 @@
-var app = angular.module('StarterApp', ['ngMaterial']);
-
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
